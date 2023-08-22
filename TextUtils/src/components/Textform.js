c5958b1c67c8fdf38fd7b868898ca064f13ddc08
Pradwin <pradwinsherigar6931@gmail.com>
2023-08-22 16:34:31 +0530

    Added extra functions for the textUtils

--- TextUtils/src/components/Textform.js
+++ TextUtils/src/components/Textform.js
@@ -1,23 +1,87 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 export default function Textform(props) {
+  const [text, setText] = useState();
+  const changeUP = () => {
+    let n = text.toUpperCase();
+    setText(n);
+  };
+  const changeDO = () => {
+    let n = text.toLowerCase();
+    setText(n);
+  };
+  const clear = () => {
+    let n = "";
+    setText(n);
+  };
+  const onhandler = (event) => {
+    setText(event.target.value);
+  };
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  };
+  const copy = () => {
+    navigator.clipboard.writeText(text);
+    alert("Copied to clipboard");
+  };
 
-    const changeUP=()=>{
-        let n= text.toUpperCase();
-        setText(n)
-    }
-    const onhandler=(event)=>{
-        setText(event.target.value);
-    }
+  let characters = text?.length;
+  let words = text?.split(" ").length;
 
-    const [text,setText] =useState();
+  const vowels = () => {
+    const count = text?.match(/[aeiou]/gi)?.length;
+    return count;
+  };
+  let resultv = vowels();
+  const consonant = () => {
+    const count = text?.match(/[^aeiou]/gi)?.length;
+    return count;
+  };
+  let resultc = consonant();
   return (
-<div>
-<div className="mb-3">
-<h1>{props.heading}</h1>
-<textarea className="form-control my-3" value={text} onChange={onhandler} placeholder='Enter your text...' id="mybox1" rows="8"></textarea>
-<button className="btn btn-primary my-2" onClick={changeUP}>Convert to UpperCase</button>
-</div>
-    </div>
-  )
+    <>
+      <div>
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control my-3"
+            value={text}
+            onChange={onhandler}
+            placeholder="Enter your text..."
+            id="mybox1"
+            rows="8"
+          ></textarea>
+          <button className="btn btn-primary my-2 mx-2" onClick={changeUP}>
+            Convert to UpperCase
+          </button>
+          <button className="btn btn-primary my-2 mx-2" onClick={changeDO}>
+            Convert to LowerCase
+          </button>
+          <button className="btn btn-primary my-2 mx-2" onClick={copy}>
+            Copy to Clipboard
+          </button>
+          <button className="btn btn-primary my-2 mx-2" onClick={speak}>
+            Speak
+          </button>
+          <button className="btn btn-primary my-2 mx-2" onClick={clear}>
+            Clear
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <h2>Text info</h2>
+        <p>
+          {" "}
+          {words} words and {characters} characters
+        </p>
+        <p>
+          {resultv} vowels and {resultc} consonent
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
